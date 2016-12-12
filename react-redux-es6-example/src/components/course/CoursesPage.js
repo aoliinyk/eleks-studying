c58dd634d4148a57048394d4db005661f896e57e
Artem Oliinyk <artem.oliinyk@eleks.com>
2016-12-12 15:51:42 +0200

    add course page and routes

---
+++ react-redux-es6-example/src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CrousesPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Courses</h1>
+        <p>React, Redux, React Router in ES6</p>
+      </div>
+    );
+  }
+}
+
+export default CrousesPage;
