1fb28f17c0d1465fa571e376d6701b6da3cd57b2
Artem Oliinyk <artem.oliinyk@eleks.com>
2016-12-14 20:11:21 +0200

    Use redux in example project

---
+++ react-redux-es6-example/src/reducers/courseReducer.js
@@ -0,0 +1,14 @@
+import * as types from '../actions/actionTypes';
+
+export default function (state = [], action) {
+  switch(action.type) {
+    case types.CREATE_COURSE:
+      return [
+        ...state,
+        Object.assign({}, action.payload)
+      ];
+
+    default:
+      return state;
+  }
+}
